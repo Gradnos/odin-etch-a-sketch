dfbeb4e4fb231b252de1e6c7660a070c453cd944
Gradnos <sdolaqidze@gmail.com>
2023-02-17 21:13:26 +0400

    Add black pen, rainbow pen, ereaser, all with wotking buttons

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector(".canvas");
 const r = document.querySelector(":root");
 let penColor = "black"; 
+let penState = "pen";
 
 let width = 700;
 let height = 700;
@@ -16,6 +17,19 @@ document.body.onmouseup = function() {
   mouseDown = 0;
 }
 
+const penStateButtons = document.querySelectorAll(".penState");
+console.log(penStateButtons);
+penStateButtons.forEach(button => {
+  button.addEventListener("click", (e) =>{
+    penStateButtons.forEach(button =>{
+      button.classList.remove("btn-selected");
+    });
+
+    button.classList.add("btn-selected");
+    penState = button.getAttribute("data-penstate");
+  });
+}); 
+
 
 r.style.setProperty("--pixelx", `${width/x}px`);
 r.style.setProperty("--pixely", `${height/y}px`);
@@ -28,12 +42,66 @@ for(let i=0; i<x; i++){
         newDiv.draggable = false;
         newDiv.addEventListener("mouseenter", (e) => {
             if(mouseDown){
-                e.target.style["background-color"]=penColor;
+                draw(e.target);
             }
         });
         newDiv.addEventListener("mousedown", (e) => {
-                e.target.style["background-color"]=penColor;
+                draw(e.target);
         });
         pixels[i][j]=newDiv;
     }
+}
+
+function draw(elemnet){
+  switch(penState){
+    case "pen":
+     // console.log(elemnet);
+      elemnet.style["background-color"]= penColor;
+      return;
+    case "rainbow":
+     // elemnet.style["background-color"]=`rgb(${rng255()},${rng255()},${rng255()})`;
+      elemnet.style["background-color"]=rainbowCol();
+
+      return;
+    case "ereaser":
+      elemnet.style["background-color"]= null;
+  }
+
+}
+
+
+function rng255(){
+  return Math.floor(Math.random() * 255);
+}
+
+let rainbowCounter = 0;
+function rainbowCol(){
+  rainbowCounter
+  console.log(rainbowCounter);
+  let col;
+  rainbowCounter++;
+  if(rainbowCounter>6) rainbowCounter = 0;
+  switch (rainbowCounter){
+    case 0:
+      col = "#FF0000";
+      return col;
+    case 1:
+        col = "#FF7F00";
+      return col;
+    case 2:
+        col = "#FFFF00";
+      return col;
+    case 3:
+        col = "#00FF00";
+      return col;
+    case 4:
+        col = "#0000FF";
+      return col;
+    case 5:
+        col = "#9400D3";
+      return col;
+    case 6:
+        col = "#4B0082";
+      return col;
+  }
 }
